78215390c9d8de8500546477f9e630ff94b9fe95
Guillaume Crabe <guillaume.crabe@ocado.com>
2025-03-16 17:53:14 -0400

    Initial commit

---
+++ celeba_keras/keras_celeba.py
@@ -0,0 +1,355 @@
+# # 🤪 Variational Autoencoders - CelebA Faces
+# In this notebook, we'll walk through the steps required to train your own variational autoencoder on the CelebA faces dataset
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import (
+    layers,
+    models,
+    callbacks,
+    utils,
+    metrics,
+    losses,
+    optimizers,
+)
+
+from scipy.stats import norm
+import pandas as pd
+
+from utils import sample_batch, display, get_vector_from_label, add_vector_to_images, morph_faces
+
+# ## 0. Parameters <a name="parameters"></a>
+
+IMAGE_SIZE = 32
+CHANNELS = 3
+BATCH_SIZE = 128
+NUM_FEATURES = 128
+Z_DIM = 200
+LEARNING_RATE = 0.0005
+EPOCHS = 10
+BETA = 2000
+LOAD_MODEL = False
+
+# ## 1. Prepare the data <a name="prepare"></a>
+
+# Load the data
+train_data = utils.image_dataset_from_directory(
+    "/home/ggrabe/celeba/data/celeba/img_align_celeba",
+    labels=None,
+    color_mode="rgb",
+    image_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    seed=42,
+    interpolation="bilinear",
+)
+
+# Preprocess the data
+def preprocess(img):
+    img = tf.cast(img, "float32") / 255.0
+    return img
+
+
+train = train_data.map(lambda x: preprocess(x))
+
+train_sample = sample_batch(train)
+
+# Show some faces from the training set
+display(train_sample, cmap=None)
+
+# ## 2. Build the variational autoencoder <a name="build"></a>
+
+class Sampling(layers.Layer):
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = K.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+# Encoder
+encoder_input = layers.Input(
+    shape=(IMAGE_SIZE, IMAGE_SIZE, CHANNELS), name="encoder_input"
+)
+x = layers.Conv2D(NUM_FEATURES, kernel_size=3, strides=2, padding="same")(
+    encoder_input
+)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(NUM_FEATURES, kernel_size=3, strides=2, padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(NUM_FEATURES, kernel_size=3, strides=2, padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2D(NUM_FEATURES, kernel_size=3, strides=2, padding="same")(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+shape_before_flattening = K.int_shape(x)[1:]  # the decoder will need this!
+
+x = layers.Flatten()(x)
+z_mean = layers.Dense(Z_DIM, name="z_mean")(x)
+z_log_var = layers.Dense(Z_DIM, name="z_log_var")(x)
+z = Sampling()([z_mean, z_log_var])
+
+encoder = models.Model(encoder_input, [z_mean, z_log_var, z], name="encoder")
+encoder.summary()
+
+# Decoder
+decoder_input = layers.Input(shape=(Z_DIM,), name="decoder_input")
+x = layers.Dense(np.prod(shape_before_flattening))(decoder_input)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Reshape(shape_before_flattening)(x)
+x = layers.Conv2DTranspose(
+    NUM_FEATURES, kernel_size=3, strides=2, padding="same"
+)(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2DTranspose(
+    NUM_FEATURES, kernel_size=3, strides=2, padding="same"
+)(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2DTranspose(
+    NUM_FEATURES, kernel_size=3, strides=2, padding="same"
+)(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+x = layers.Conv2DTranspose(
+    NUM_FEATURES, kernel_size=3, strides=2, padding="same"
+)(x)
+x = layers.BatchNormalization()(x)
+x = layers.LeakyReLU()(x)
+decoder_output = layers.Conv2DTranspose(
+    CHANNELS, kernel_size=3, strides=1, activation="sigmoid", padding="same"
+)(x)
+decoder = models.Model(decoder_input, decoder_output)
+decoder.summary()
+
+class VAE(models.Model):
+    def __init__(self, encoder, decoder, **kwargs):
+        super(VAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.total_loss_tracker = metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = metrics.Mean(name="kl_loss")
+
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+
+    def call(self, inputs):
+        """Call the model on a particular input."""
+        z_mean, z_log_var, z = encoder(inputs)
+        reconstruction = decoder(z)
+        return z_mean, z_log_var, reconstruction
+
+    def train_step(self, data):
+        """Step run during training."""
+        with tf.GradientTape() as tape:
+            z_mean, z_log_var, reconstruction = self(data, training=True)
+            reconstruction_loss = tf.reduce_mean(
+                BETA * losses.MeanSquaredError()(data, reconstruction) #create an instance of the class then call it.
+            )
+            kl_loss = tf.reduce_mean(
+                tf.reduce_sum(
+                    -0.5
+                    * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)),
+                    axis=1,
+                )
+            )
+            total_loss = reconstruction_loss + kl_loss
+
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+    def test_step(self, data):
+        """Step run during validation."""
+        if isinstance(data, tuple):
+            data = data[0]
+
+        z_mean, z_log_var, reconstruction = self(data)
+        reconstruction_loss = tf.reduce_mean(
+            BETA * losses.MeanSquaredError()(data, reconstruction) #create an instance of the class then call it.
+        )
+        kl_loss = tf.reduce_mean(
+            tf.reduce_sum(
+                -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var)),
+                axis=1,
+            )
+        )
+        total_loss = reconstruction_loss + kl_loss
+
+        return {
+            "loss": total_loss,
+            "reconstruction_loss": reconstruction_loss,
+            "kl_loss": kl_loss,
+        }
+
+# Create a variational autoencoder
+vae = VAE(encoder, decoder)
+
+# ## 3. Train the variational autoencoder <a name="train"></a>
+
+# Compile the variational autoencoder
+optimizer = optimizers.Adam(learning_rate=LEARNING_RATE)
+vae.compile(optimizer=optimizer)
+
+# Create a model save checkpoint
+model_checkpoint_callback = callbacks.ModelCheckpoint(
+    filepath="./checkpoint/my_model.keras", #add .keras extention.
+    save_weights_only=False,
+    save_freq="epoch",
+    monitor="loss",
+    mode="min",
+    save_best_only=True,
+    verbose=0,
+)
+
+tensorboard_callback = callbacks.TensorBoard(log_dir="./logs")
+
+
+class ImageGenerator(callbacks.Callback):
+    def __init__(self, num_img, latent_dim):
+        self.num_img = num_img
+        self.latent_dim = latent_dim
+
+    def on_epoch_end(self, epoch, logs=None):
+        random_latent_vectors = tf.random.normal(
+            shape=(self.num_img, self.latent_dim)
+        )
+        generated_images = self.model.decoder(random_latent_vectors)
+        generated_images *= 255
+        generated_images.numpy()
+        for i in range(self.num_img):
+            img = utils.array_to_img(generated_images[i])
+            img.save("./output/generated_img_%03d_%d.png" % (epoch, i))
+
+# Load old weights if required
+if LOAD_MODEL:
+    vae.load_weights("./models/vae")
+    tmp = vae.predict(train.take(1))
+
+vae.fit(
+    train,
+    epochs=EPOCHS,
+    callbacks=[
+        model_checkpoint_callback,
+        tensorboard_callback,
+        ImageGenerator(num_img=10, latent_dim=Z_DIM),
+    ],
+)
+
+# Save the final models
+vae.save("./models/vae")
+encoder.save("./models/encoder")
+decoder.save("./models/decoder")
+
+# ## 3. Reconstruct using the variational autoencoder <a name="reconstruct"></a>
+
+# Select a subset of the test set
+batches_to_predict = 1
+example_images = np.array(
+    list(train.take(batches_to_predict).get_single_element())
+)
+
+# Create autoencoder predictions and display
+z_mean, z_log_var, reconstructions = vae.predict(example_images)
+print("Example real faces")
+display(example_images)
+print("Reconstructions")
+display(reconstructions)
+
+# ## 4. Latent space distribution
+
+_, _, z = vae.encoder.predict(example_images)
+
+x = np.linspace(-3, 3, 100)
+
+fig = plt.figure(figsize=(20, 5))
+fig.subplots_adjust(hspace=0.6, wspace=0.4)
+
+for i in range(50):
+    ax = fig.add_subplot(5, 10, i + 1)
+    ax.hist(z[:, i], density=True, bins=20)
+    ax.axis("off")
+    ax.text(
+        0.5, -0.35, str(i), fontsize=10, ha="center", transform=ax.transAxes
+    )
+    ax.plot(x, norm.pdf(x))
+
+plt.show()
+
+# ## 5. Generate new faces <a name="decode"></a>
+
+# Sample some points in the latent space, from the standard normal distribution
+grid_width, grid_height = (10, 3)
+z_sample = np.random.normal(size=(grid_width * grid_height, Z_DIM))
+
+# Decode the sampled points
+reconstructions = decoder.predict(z_sample)
+
+# Draw a plot of decoded images
+fig = plt.figure(figsize=(18, 5))
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+
+# Output the grid of faces
+for i in range(grid_width * grid_height):
+    ax = fig.add_subplot(grid_height, grid_width, i + 1)
+    ax.axis("off")
+    ax.imshow(reconstructions[i, :, :])
+
+# ## 6. Manipulate the images <a name="manipulate"></a>
+
+# Load the label dataset
+attributes = pd.read_csv("/home/ggrabe/celeba/data/celeba/list_attr_celeba.txt")
+print(attributes.columns)
+attributes.head()
+
+# Load the face data with label attached
+LABEL = "Blond_Hair"  # <- Set this label
+labelled_test = utils.image_dataset_from_directory(
+    "/home/ggrabe/celeba/data/celeba/img_align_celeba",
+    labels=attributes[LABEL].tolist(),
+    color_mode="rgb",
+    image_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    shuffle=True,
+    seed=42,
+    validation_split=0.2,
+    subset="validation",
+    interpolation="bilinear",
+)
+
+labelled = labelled_test.map(lambda x, y: (preprocess(x), y))
+
+# Find the attribute vector
+attribute_vec = get_vector_from_label(labelled, vae, Z_DIM, LABEL)
+
+# Add vector to images
+add_vector_to_images(labelled, vae, attribute_vec)
+
+morph_faces(labelled, vae)
+
+
---
+++ celeba_pytorch.py
@@ -0,0 +1,247 @@
+import os
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torchinfo import summary
+
+## Define my encoder model
+
+def init_weights(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
+        nn.init.xavier_normal_(m.weight)  # Xavier/Glorot normal initialization
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)  # Initialize biases to zero
+
+class Sampling(nn.Module):
+    """
+    Sampling layer for VAE that implements the reparameterization trick
+    """
+    def __init__(self):
+        super(Sampling, self).__init__()
+
+    def forward(self, z_mean, z_log_var):
+        batch_size = z_mean.size(0)
+        latent_dim = z_mean.size(1)
+
+        # Sample from standard normal distribution
+        epsilon = torch.randn(batch_size, latent_dim, device=z_mean.device)
+
+        # Reparameterization trick: z = μ + σ * ε (where σ = exp(log_var/2))
+        return z_mean + torch.exp(0.5 * z_log_var) * epsilon
+
+class EncoderModel(nn.Module):
+    def __init__(self, latent_dim=200):
+        super(EncoderModel, self).__init__()
+
+        # Define the CNN layers using Sequential
+        self.encoder_cnn = nn.Sequential(
+            nn.Conv2d(3, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.Flatten()
+        )
+
+        # Latent space
+        self.z_mean = nn.Linear(512, latent_dim)
+        self.z_log_var = nn.Linear(512, latent_dim)
+        self.sampling = Sampling()
+
+    def forward(self, x):
+        x = self.encoder_cnn(x)
+
+        # Generate latent parameters
+        z_mean = self.z_mean(x)
+        z_log_var = self.z_log_var(x)
+
+        # Sample from the distribution
+        z = self.sampling(z_mean, z_log_var)
+
+        return z_mean, z_log_var, z
+
+class DecoderModel(nn.Module):
+    def __init__(self, latent_dim=200):
+        super(DecoderModel, self).__init__()
+
+        # Latent dim conversion
+        self.dense = nn.Linear(latent_dim, 512)
+
+        self.batch_norm_dense = nn.BatchNorm1d(512)
+
+        self.leaky_relu_dense = nn.LeakyReLU()
+
+         # Define the CNN layers using Sequential
+        self.decoder_conv = nn.Sequential(
+            nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(128, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+
+            nn.ConvTranspose2d(128, 3, kernel_size=3, stride=1, padding=1, output_padding=0),
+        )
+
+
+    def forward(self, x):
+        # Linear projection and reshape
+
+        x = self.dense(x)
+        x = self.batch_norm_dense(x)
+        x = self.leaky_relu_dense(x)
+
+        # Reshape to match the expected input for transposed convolutions
+        # The shape should match the flattened output of the encoder's last conv layer
+        # If encoder output was 2x2x256 before flattening
+        x = x.view(-1, 128, 2, 2)  # [-1, channels, height, width]
+
+        # Apply transposed convolutions
+        x = self.decoder_conv(x)
+
+        x = torch.sigmoid(x)
+
+        return x
+
+class VAE(nn.Module):
+    def __init__(self, latent_dim=200):
+        super(VAE, self).__init__()
+
+        self.encoder = EncoderModel(latent_dim)
+        self.decoder = DecoderModel(latent_dim)
+
+def train_step(model, optimizer, x):
+    # Zero the gradients
+    optimizer.zero_grad()
+
+    #forward
+    z_mean, z_log_var, z = model.encoder.forward(x)
+    output = model.decoder.forward(z)
+
+    # Define KL loss
+    kl_loss = -0.5 * torch.sum(1 + z_log_var - z_mean.pow(2) - z_log_var.exp())
+
+    # Define the binary cross entropy
+    reconstruction_loss = 2000 * F.mse_loss(output, x, reduction='mean')
+
+    total_loss = kl_loss + reconstruction_loss
+
+    total_loss.backward()
+
+    optimizer.step()
+
+    return total_loss.item(), reconstruction_loss.item(), kl_loss.item()
+
+def train(model, dataloader, epochs=100):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+
+    loss_function = lambda output, x: 2000 * F.mse_loss(output, x, reduction='mean') #Match Keras loss.
+
+    for epoch in range(epochs):
+        #Set the model to training mode (Under the hood, set compute gradients to true)
+        model.train()
+
+        train_loss = 0
+        recon_loss_total = 0
+        kl_loss_total = 0
+
+        for batch_idx, data in enumerate(dataloader):
+            x, _ = data
+            x = x.to(device)
+
+            loss, recon_loss, kl_loss = train_step(model, optimizer, x)
+
+            train_loss += loss
+            recon_loss_total += recon_loss
+            kl_loss_total += kl_loss
+
+            if batch_idx % 100 == 0:
+                print(f'Epoch: {epoch}, Batch: {batch_idx}, Loss: {loss:.4f}, '
+                      f'Recon: {recon_loss:.4f}, KL: {kl_loss:.4f}')
+
+        avg_loss = train_loss / len(dataloader)
+        avg_recon = recon_loss_total / len(dataloader)
+        avg_kl = kl_loss_total / len(dataloader)
+
+        print(f'====> Epoch: {epoch} Average loss: {avg_loss:.4f}, '
+              f'Recon: {avg_recon:.4f}, KL: {avg_kl:.4f}')
+
+def display(
+    images, n=10, size=(20, 3), cmap="gray_r", as_type="float32", save_to=None
+):
+    """
+    Displays n random import matplotlib.pyplot as pltimages from each one of the supplied arrays.
+    """
+    if isinstance(images, torch.Tensor):
+        images = images.cpu().numpy()
+
+    images = images.transpose(0, 2, 3, 1)
+
+    plt.figure(figsize=size)
+    for i in range(n):
+        _ = plt.subplot(1, n, i + 1)
+        plt.imshow(images[i].astype(as_type), cmap=cmap)
+        plt.axis("off")
+
+    if save_to:
+        plt.savefig(save_to)
+        print(f"\nSaved to {save_to}")
+
+    plt.show()
+
+if __name__ == "__main__":
+    data_dir = './data'  # Correct relative path
+    transform = transforms.Compose([
+        transforms.Resize((32, 32)),
+        transforms.ToTensor(),
+    ])
+    print(f"Dataset root: {data_dir}")
+    print(f"Contents of data/celeba: {os.listdir(os.path.join(data_dir, 'celeba'))}")
+    celeba_dataset = datasets.CelebA(
+        root=data_dir,
+        split='all',
+        target_type='attr',
+        transform=transform,
+        download=False
+    )
+    dataloader = DataLoader(celeba_dataset, batch_size=128, shuffle=True)
+
+    data_batch, labels_batch = next(iter(dataloader))
+
+    display(data_batch)
+
+    model = VAE()
+    init_weights(model)
+
+    summary(model.encoder, input_size=(1, 3, 32, 32))
+    summary(model.decoder, input_size=(1, 200))
+
+    train(model, dataloader, epochs=10)
